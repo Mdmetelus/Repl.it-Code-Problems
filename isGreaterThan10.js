ba44c3a085e94256a5b49a5e73bf21ba9cf2b05c
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-18 20:24:55 -0500

    "isGreaterThan10 completed"

---
+++ isGreaterThan10.js
@@ -0,0 +1,23 @@
+// isGreaterThan10
+
+// Instructions from your teacher:
+// Write a function called "isGreaterThanTen".
+
+// Given a number, "isGreaterThanTen" returns whether it is greater than 10.
+
+// var output = isGreaterThanTen(11);
+// console.log(output); // --> true
+
+function isGreaterThanTen(num) {
+    // your code here
+    if(num > 10) {
+        return true;
+        
+    } else {
+        return false;
+    }
+        
+  }
+  
+  var output = isGreaterThanTen(11);
+  console.log(output); // --> true
