bf3a00d2d5194b6b51aca174015ced98d9671d1f
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-18 20:30:21 -0500

    "error"

---
+++ isLessThan30.js
@@ -0,0 +1,19 @@
+// isLessThan30
+
+// Instructions from your teacher:
+//  Write a function called "isLessThan30".
+
+// Given a number, "isLessThan30" returns whether the given number is less than 30.
+
+// var output = isLessThan30(9);
+// console.log(output); // --> true
+
+function isLessThan30(num) {
+    // your code here
+    return num < 30 ? true : false;
+  }
+  
+  //must use a ternary operator
+  
+  var output = isLessThan30(9);
+  console.log(output); // --> true
