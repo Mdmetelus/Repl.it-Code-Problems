a30bc8a7d7ed4827848d3c6533881b189e69edb3
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-18 20:20:27 -0500

    "getLengthOfTwoWords completed"

---
+++ getLengthOfTwoWords.js
@@ -0,0 +1,17 @@
+// getLengthOfTwoWords
+
+// Instructions from your teacher:
+//  Write a function called "getLengthOfTwoWords".
+
+// Given 2 words, "getLengthOfTwoWords" returns the sum of their lengths.
+
+// var output = getLengthOfTwoWords('some', 'words');
+// console.log(output); // --> 9
+
+function getLengthOfTwoWords(word1, word2) {
+    // your code here
+    return word1.length + word2.length;
+  }
+  
+  var output = getLengthOfTwoWords('some', 'words');
+  console.log(output); // --> 9
