c750a581a5911026f24fc5cc95ed7306e0e8a452
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-28 00:19:57 -0500

    "is Even and greater tha ten

---
+++ isEvenAndGreaterThanTen.js
@@ -0,0 +1,17 @@
+// isEvenAndGreaterThanTen
+
+// Instructions from your teacher:
+// Write a function called "isEvenAndGreaterThanTen".
+
+// Given a number, "isEvenAndGreaterThanTen" returns whether it is both even and greater than 10.
+
+// var output = isEvenAndGreaterThanTen(13);
+// console.log(output); // --> false
+
+function isEvenAndGreaterThanTen(num) {
+    // your code here
+    
+     return  num % 2 === 0 && num > 10 === true ? true : false;
+  }
+  
+  isEvenAndGreaterThanTen(13);
