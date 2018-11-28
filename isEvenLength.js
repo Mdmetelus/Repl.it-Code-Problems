7a517ad4081fae080f12d7a500bd5da057b13629
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-28 00:20:10 -0500

    "even completed"

---
+++ isEvenLength.js
@@ -0,0 +1,16 @@
+// isEvenLength
+
+// Instructions from your teacher:
+//  Write a function called "isEvenLength".
+
+// Given a word, "isEvenLength" returns whether the length of the word is even.
+
+// var output = isEvenLength('wow');
+// console.log(output); // --> false
+
+function isEvenLength(word) {
+    // your code here
+    return word.length % 2 === 0 ? true : false
+  }
+  
+  isEvenLength('wow');
