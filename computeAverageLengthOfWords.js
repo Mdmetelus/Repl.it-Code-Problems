677b7d4265368b148d0cfd1d6911f9a2f24bce7a
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-28 00:30:18 -0500

    "compute aavg length of words completed"

---
+++ computeAverageLengthOfWords.js
@@ -0,0 +1,16 @@
+// computeAverageLengthOfWords
+
+// Instructions from your teacher:
+// Write a function called "computeAverageLengthOfWords".
+
+// Given two words, "computeAverageLengthOfWords" returns the average of their lengths.
+
+// var output = computeAverageLengthOfWords('code', 'programs');
+// console.log(output); // --> 6
+
+function computeAverageLengthOfWords(word1, word2) {
+    // your code here
+    return (word1.length + word2.length) /2
+  }
+  
+  computeAverageLengthOfWords('code', 'programs');
