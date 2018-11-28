e958766c993208179bfd9317b9289096e35c964d
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-28 00:29:35 -0500

    "average completed."

---
+++ average.js
@@ -0,0 +1,17 @@
+// average
+// Instructions from your teacher:
+//  Write a function called "average".
+
+// Given two numbers, "average" returns their average.
+
+// var output = average(4, 6);
+// console.log(output); // --> 5
+
+function average(num1, num2) {
+    // your code here
+    
+    return (num1 + num2)/2;
+  
+  }
+  
+  average(4, 6);
