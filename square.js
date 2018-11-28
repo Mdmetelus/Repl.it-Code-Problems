15b1af25af016d363f7c8c6a6a4fd842647a70a8
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-28 00:35:35 -0500

    "sqr completed"

---
+++ square.js
@@ -0,0 +1,17 @@
+// square
+
+// Instructions from your teacher:
+//  Write a function called "square".
+
+// Given a number, "square" should return the square of the given number.
+
+// var output = square(5);
+// console.log(output); // --> 25
+
+function square(num) {
+    // your code here
+  //   return num *num 
+      return num**2
+  }
+  
+  square(5);
