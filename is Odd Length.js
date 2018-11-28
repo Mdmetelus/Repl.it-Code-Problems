5e96fad1cf217262e7b1ceb83deb2de52fbff910
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-28 00:12:34 -0500

    "odd length and either even are complete"

---
+++ is Odd Length.js
@@ -0,0 +1,15 @@
+// isOddLength
+// Instructions from your teacher:
+//  Write a function called "isOddLength".
+
+// Given a word, "isOddLength" returns whether the length of the given word is odd.
+
+// var output = isOddLength('special');
+// console.log(output); // --> true
+
+function isOddLength(word) {
+    // your code here
+    return word.length % 2 === 1 ? true : false
+  }
+  
+  isOddLength('special');
---
+++ isEitherEven.js
@@ -0,0 +1,16 @@
+// isEitherEven
+
+// Instructions from your teacher:
+// Write a function called "isEitherEven".
+
+// Given two numbers, "isEitherEven" returns whether or not either one of the given numbers is even.
+
+// var output = isEitherEven(1, 4);
+// console.log(output); // --> true
+
+function isEitherEven(num1, num2) {
+    // your code here
+    return num1 % 2 === 0 || num2 % 2 === 0 ? true : false;
+    
+  }
+  isEitherEven(1, 4);
