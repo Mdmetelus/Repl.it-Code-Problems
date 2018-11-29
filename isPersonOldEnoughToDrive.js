83424f9199f9c0eb42fcb98ccd74fecef485cc90
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-28 22:56:01 -0500

    "completed"

---
+++ isPersonOldEnoughToDrive.js
@@ -0,0 +1,24 @@
+// isPersonOldEnoughToDrive
+
+// Instructions from your teacher:
+//  Write a function called "isPersonOldEnoughToDrive".
+
+// Given a "person" object, that contains an "age" property, "isPersonOldEnoughToDrive" returns whether the given person is old enough to drive.
+
+// Notes:* The legal driving age in the United States is 16.
+
+// var obj = {
+//   age: 16
+// };
+// var output = isPersonOldEnoughToDrive(obj);
+// console.log(output); // --> true
+
+function isPersonOldEnoughToDrive(person) {
+    // your code here
+    return person.age > 15 ? true : false
+  }
+  
+  var obj = {
+    age: 16
+  };
+  isPersonOldEnoughToDrive(obj);
