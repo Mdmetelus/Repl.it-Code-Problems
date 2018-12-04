ff1a1281d608b883dc4de2eb6aa1828494b8aad0
Mr. Metelus <Mdmetelus@gmail.com>
2018-12-03 21:46:19 -0500

    "computepower."

---
+++ computePower.js
@@ -0,0 +1,16 @@
+// computePower
+// Instructions from your teacher:
+// Write a function called "computePower"
+// .
+// Given a number and an exponent, "computePower" returns the given number, raised to the given exponent. 
+
+// var output = computePower(2, 3);
+// console.log(output); // --> 8
+
+function computePower(num, exponent) {
+    // your code here
+    return Math.pow(num, exponent);
+    return num**exponent;
+  }
+  
+  computePower(2, 3);
