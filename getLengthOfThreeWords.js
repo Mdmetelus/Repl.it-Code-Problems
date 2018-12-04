f2d1d153d04437d7cb9f41e82c5aea367a582a0e
Mr. Metelus <Mdmetelus@gmail.com>
2018-12-03 21:49:40 -0500

    "getLengthOfThreeWords completed"

---
+++ getLengthOfThreeWords.js
@@ -0,0 +1,16 @@
+// getLengthOfThreeWords
+
+// Instructions from your teacher:
+// Write a function called "getLengthOfThreeWords".
+
+// Given 3 words, "getLengthOfThreeWords" returns the sum of their lengths.
+
+// var output = getLengthOfThreeWords('some', 'other', 'words');
+// console.log(output); // --> 14
+
+function getLengthOfThreeWords(word1, word2, word3) {
+    // your code here
+    return word1.length + word2.length + word3.length;
+    
+  }
+  getLengthOfThreeWords('some', 'other', 'words');
